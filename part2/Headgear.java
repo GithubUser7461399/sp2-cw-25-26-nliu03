be9d4b62cedb6f9420ff313c4a2366047ca35200
GithubUser7461399 <lnicholas2025@outlook.com>
2025-11-20 07:30:29 +0000

    Created javadoc for Headgear

--- part2/Headgear.java
+++ part2/Headgear.java
@@ -1,3 +1,10 @@
+/**
+*
+* Interface Headgear for which all other classes in the Part 2 coursework must implement from.
+* The Headgear class requires all headgear have an implementation for the getValue() method.
+* @author Nicholas Liu
+*
+*/
 interface Headgear {
     double value = 0.0;
     double getValue();
