e046aa4c0b981118b2ae33f6615625c30aa1e032
GithubUser7461399 <lnicholas2025@outlook.com>
2025-11-20 07:21:18 +0000

    Created constructor and implementation for getValue in ProtectiveHeadgear class, changed return type for getValue in Headgear class

--- part2/Headgear.java
+++ part2/Headgear.java
@@ -1,4 +1,4 @@
 interface Headgear {
     double value = 0.0;
-    void getValue();
+    double getValue();
 }
--- part2/ProtectiveHeadgear.java
+++ part2/ProtectiveHeadgear.java
@@ -1,3 +1,11 @@
 abstract class ProtectiveHeadgear implements Headgear {
-    
+    protected double value;
+    protected double protectionFactor;
+    public ProtectiveHeadgear(double value, double protectionFactor) {
+        this.value = value;
+        this.protectionFactor = protectionFactor;
+    }
+    public double getValue() {
+        return value;
+    }
 }
