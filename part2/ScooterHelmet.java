aaa615266540936f652f9954be2cb5cfadfd6334
GithubUser7461399 <lnicholas2025@outlook.com>
2025-11-20 07:42:44 +0000

    Created constructor, implemented getValue, mutator and accessor for visor property in ScooterHelmet

--- part2/ScooterHelmet.java
+++ part2/ScooterHelmet.java
@@ -1,3 +1,16 @@
 public class ScooterHelmet extends ProtectiveHeadgear {
-    
+    private boolean visor;
+    public ScooterHelmet(double value, double protectionFactor, boolean visor) {
+        super(value, protectionFactor);
+        this.visor = visor;
+    }
+    public double getValue() {
+        return (visor)? 80.0 : 160.0 + protectionFactor * 400;
+    }
+    public boolean getVisor() {
+        return visor;
+    }
+    public void setVisor(boolean visor) {
+        this.visor = visor;
+    }
 }
