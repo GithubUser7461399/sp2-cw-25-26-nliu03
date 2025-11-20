d60649c9c50b2b1c0030acf921dec42ac060b0a1
GithubUser7461399 <lnicholas2025@outlook.com>
2025-11-20 08:00:08 +0000

    Added javadoc and error handling in BobbleHat class

--- part2/BobbleHat.java
+++ part2/BobbleHat.java
@@ -1,8 +1,20 @@
+/**
+*
+* Concrete class BobbleHat extends abstract class ProtectiveHeadgear.
+* BobbleHat has property bobbleDiameter, which is the diameter of its bobble in millimetres.bobbleDiameter has an accessor and mutator.
+* bobbleDiameter is used in the BobbleHat class' implementation for getValue().
+* @author Nicholas Liu
+*
+*/
 public class BobbleHat extends ProtectiveHeadgear {
     private double bobbleDiameter;
     public BobbleHat(double value, double protectionFactor, double bobbleDiameter) {
         super(value, protectionFactor);
-        this.bobbleDiameter = bobbleDiameter;
+        if (bobbleDiameter >= 0) {
+            this.bobbleDiameter = bobbleDiameter;
+        } else {
+            throw new IllegalArgumentException("Bobble diameter can't be below zero.");
+        }
     }
     public double getValue() {
         return bobbleDiameter * 4 * protectionFactor;
@@ -11,6 +23,10 @@ public class BobbleHat extends ProtectiveHeadgear {
         return bobbleDiameter;
     }
     public void setBobbleDiameter(double bobbleDiameter) {
-        this.bobbleDiameter = bobbleDiameter;
+        if (bobbleDiameter >= 0) {
+            this.bobbleDiameter = bobbleDiameter;
+        } else {
+            throw new IllegalArgumentException("Bobble diameter can't be below zero.");
+        }
     }
 }
