5e00613906c9c9dcf7650ec8a87bce246fad24d5
GithubUser7461399 <lnicholas2025@outlook.com>
2025-11-20 07:53:23 +0000

    Created constructor, implementation for getValue(), accessor and mutator for bobbleDiameter property in BobbleHat.

--- part2/BobbleHat.java
+++ part2/BobbleHat.java
@@ -1,3 +1,16 @@
 public class BobbleHat extends ProtectiveHeadgear {
-    
+    private double bobbleDiameter;
+    public BobbleHat(double value, double protectionFactor, double bobbleDiameter) {
+        super(value, protectionFactor);
+        this.bobbleDiameter = bobbleDiameter;
+    }
+    public double getValue() {
+        return bobbleDiameter * 4 * protectionFactor;
+    }
+    public double getBobbleDiameter() {
+        return bobbleDiameter;
+    }
+    public void setBobbleDiameter(double bobbleDiameter) {
+        this.bobbleDiameter = bobbleDiameter;
+    }
 }
