2e3a49d447be230e983daaafe803d64326ea9b09
GithubUser7461399 <lnicholas2025@outlook.com>
2025-11-20 07:26:52 +0000

    Created implementation for getValue in Crown class based on variable jewels. Added accessor and mutator for jewels property

--- part2/Crown.java
+++ part2/Crown.java
@@ -1,3 +1,14 @@
 public class Crown implements Headgear {
-    
+    private int jewels;
+    public double getValue() {
+        return jewels * 200000;
+    }
+    public int getJewels() {
+        return jewels;
+    }
+    public void setJewels(int jewels) {
+        if (jewels >= 0) {
+            this.jewels = jewels;
+        }
+    }
 }
